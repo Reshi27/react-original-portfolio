dd26fe0e186807f4661588d86d897474da0b3f69
Mahareshi Wahyuaji <m.reshi1998@gmail.com>
2022-11-29 21:17:36 +0700

    - added login page design

---
+++ src/components/common/LoginForm/LoginStyle.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  background-color: #0f0f0f;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+`;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { LoginForm } from "../components/common/LoginForm/LoginForm";
+
+function LoginPage() {
+  return (
+    <Fragment>
+      <LoginForm></LoginForm>
+    </Fragment>
+  );
+}
+
+export default LoginPage;
