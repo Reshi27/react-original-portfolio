fe59fbac2d929357a7f73ae0845056d70fa998c3
Reshi27 <m.reshi1998@gmail.com>
2023-01-19 11:09:26 +0700

    -fixed responsive in hero section -fixed responsive of the paper in blog section

--- src/components/layout/Hero/HeroSection.tsx
+++ src/components/layout/Hero/HeroSection.tsx
@@ -1,31 +1,16 @@
 import { Container, Text, Title } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import React, { Fragment } from "react";
+import { HeroSpanTitle, HeroText, HeroTitle } from "../Blog/BlogSection.style";
 
 const HeroSection: React.FC = () => {
   const isMobile = useMediaQuery("(min-width: 579px)");
   return (
     <Fragment>
       <Container mt={100}>
-        <Title size={isMobile ? 120 : 60} sx={{ fontFamily: "Roboto" }}>
-          Mahareshi
-        </Title>
-        <Title
-          size={isMobile ? 120 : 60}
-          ml={isMobile ? 120 : 60}
-          sx={{ fontFamily: "Roboto" }}
-        >
-          Wahyuaji
-        </Title>
-
-        <Text
-          size={isMobile ? 36 : 16}
-          sx={{ fontFamily: "Raleway", letterSpacing: 10 }}
-          c="cyan"
-          mt={20}
-        >
-          FRONT-END DEVELOPER
-        </Text>
+        <HeroTitle>Mahareshi</HeroTitle>
+        <HeroSpanTitle>Wahyuaji</HeroSpanTitle>
+        <HeroText>FRONT-END DEVELOPER</HeroText>
       </Container>
     </Fragment>
   );
