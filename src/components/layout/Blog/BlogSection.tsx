c1d74f2d3b34357480c3c9ed4d88de25f118999a
Mahareshi Wahyuaji <m.reshi1998@gmail.com>
2023-01-22 17:40:39 +0700

    -commiting

--- src/components/layout/Blog/BlogSection.tsx
+++ src/components/layout/Blog/BlogSection.tsx
@@ -8,8 +8,8 @@ const BlogSection = () => {
         <BlogContainer>
           <Title align="center">This is a Blog Section</Title>
 
-          <Grid mt={36}>
-            <Grid.Col md={6} lg={3} mt={10}>
+          <Grid mt={36} gutter="xl">
+            <Grid.Col md={12} lg={4} mt={10}>
               <Card shadow="md" p="xl">
                 <Card.Section>
                   <Image
@@ -29,7 +29,7 @@ const BlogSection = () => {
                 </Text>
               </Card>
             </Grid.Col>
-            <Grid.Col md={6} lg={3} mt={10}>
+            <Grid.Col md={12} lg={4} mt={10}>
               <Card shadow="md" p="xl">
                 <Card.Section>
                   <Image
@@ -49,27 +49,7 @@ const BlogSection = () => {
                 </Text>
               </Card>
             </Grid.Col>
-            <Grid.Col md={6} lg={3} mt={10}>
-              <Card shadow="md" p="xl">
-                <Card.Section>
-                  <Image
-                    src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-                    height={160}
-                    alt="No way!"
-                  />
-                </Card.Section>
-
-                <Text weight={500} size="lg" mt="md">
-                  You&apos;ve won a million dollars in cash!
-                </Text>
-
-                <Text mt="xs" color="dimmed" size="sm">
-                  Please click anywhere on this card to claim your reward, this
-                  is not a fraud, trust us
-                </Text>
-              </Card>
-            </Grid.Col>
-            <Grid.Col md={6} lg={3} mt={10}>
+            <Grid.Col sm={12} md={12} lg={4} mt={10}>
               <Card shadow="md" p="xl">
                 <Card.Section>
                   <Image
--- src/components/layout/Footer/FooterSection.tsx
+++ src/components/layout/Footer/FooterSection.tsx
@@ -2,7 +2,7 @@ import { Grid, Text } from "@mantine/core";
 import React, { Fragment } from "react";
 import signature from "../../../assets/signature.png";
 import { Signature } from "./FooterSection.style";
-import { IconBrandGithub } from "@tabler/icons";
+import { useMediaQuery } from "react-responsive";
 const FooterSection: React.FC = () => {
   return (
     <Fragment>
